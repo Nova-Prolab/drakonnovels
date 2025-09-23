e2fd646f5a42c05fcc150812c12606861729e612
Alt 2 <alt053872@gmail.com>
2025-09-23 06:42:23 +0000

    Vale, ahí me parece perfecto, solo muevelo un poquito, pero solo un poqu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ export function Header() {
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur-sm">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         <Link href="/" className="relative flex items-center h-full -ml-10 mt-2">
-          <Image src="https://i.imgur.com/18GTnzt.png" alt="DrakonInk Logo" width={604} height={160} className="w-auto" style={{ height: '180px' }}/>
+          <Image src="https://i.imgur.com/18GTnzt.png" alt="DrakonInk Logo" width={800} height={213} className="w-auto" style={{ height: '210px' }}/>
         </Link>
         <SettingsControls />
       </div>
