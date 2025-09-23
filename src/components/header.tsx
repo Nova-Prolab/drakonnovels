7e7ad938a44de200695b6a545590c618d4c14b48
Alt 2 <alt053872@gmail.com>
2025-09-23 06:36:51 +0000

    Haz un poco más grande el logo y ponlo más para la derecha

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,8 +6,8 @@ export function Header() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur-sm">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
-        <Link href="/" className="relative flex items-center">
-          <Image src="https://i.imgur.com/18GTnzt.png" alt="DrakonInk Logo" width={528} height={140} className="w-auto" style={{ height: '140px' }}/>
+        <Link href="/" className="relative flex items-center ml-4">
+          <Image src="https://i.imgur.com/18GTnzt.png" alt="DrakonInk Logo" width={604} height={160} className="w-auto" style={{ height: '160px' }}/>
         </Link>
         <SettingsControls />
       </div>
