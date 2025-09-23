8ea11da080e03869283b4b42745c880d374fbf6a
Alt 2 <alt053872@gmail.com>
2025-09-23 16:18:20 +0000

    Lo mismo... Cuando clickeo una novela no aparece, solo aparece cuando YA

--- src/app/novels/[novelId]/[chapterId]/loading.tsx
+++
@@ -1,5 +0,0 @@
-import Loading from '@/app/loading';
-
-export default function ChapterLoading() {
-  return <Loading />;
-}
--- src/app/novels/[novelId]/loading.tsx
+++
@@ -1,5 +0,0 @@
-import Loading from '@/app/loading';
-
-export default function NovelLoading() {
-  return <Loading />;
-}
